c08cfb61ba00518b9cf1d4b0e23bcb4a0f73f1da
Ivan Sy <ivansyrups@gmail.com>
2023-07-14 01:46:37 -0700

    Weather and Quotes API added

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var path = require("path");
+const request = require('request');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.status(200);
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
 
-app.get('/weather', function (req, res) {
-    res.sendFile(path.join(__dirname, '/index.html/'));
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/images', express.static(__dirname + 'public/images'));
+
+app.get('', async function (req, res) {
+    res.status(200);
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 app.listen(3000, function () {
